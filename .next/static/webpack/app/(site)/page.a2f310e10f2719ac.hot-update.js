ccba19de2c8c91a3df90cf2d2b1ccc1e7d73fa78
Natalia Kudriavtseva <natalia.kudriavtseva@decathlon.com>
2023-07-06 16:27:04 +0300

    add next files

---
+++ .next/static/webpack/app/(site)/page.a2f310e10f2719ac.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/(site)/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FHeader.tsx&modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FListItem.tsx&server=false!":
+/*!***************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FHeader.tsx&modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FListItem.tsx&server=false! ***!
+  \***************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./components/Header.tsx */ \"(app-client)/./components/Header.tsx\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./components/ListItem.tsx */ \"(app-client)/./components/ListItem.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRlVzZXJzJTJGTkFUQUxJQSUyRkRvY3VtZW50cyUyRnNwb3RpZnktY2xvbmUlMkZjb21wb25lbnRzJTJGSGVhZGVyLnRzeCZtb2R1bGVzPSUyRlVzZXJzJTJGTkFUQUxJQSUyRkRvY3VtZW50cyUyRnNwb3RpZnktY2xvbmUlMkZjb21wb25lbnRzJTJGTGlzdEl0ZW0udHN4JnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEsaUtBQWlHO0FBQ2pHIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/MjkzNiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIi9Vc2Vycy9OQVRBTElBL0RvY3VtZW50cy9zcG90aWZ5LWNsb25lL2NvbXBvbmVudHMvSGVhZGVyLnRzeFwiKTtcbmltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL1VzZXJzL05BVEFMSUEvRG9jdW1lbnRzL3Nwb3RpZnktY2xvbmUvY29tcG9uZW50cy9MaXN0SXRlbS50c3hcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FHeader.tsx&modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FListItem.tsx&server=false!\n"));
+
+/***/ })
+
+});
---
+++ .next/static/webpack/app/(site)/page.d976489a1610a997.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/(site)/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FListItem.tsx&modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FHeader.tsx&server=false!":
+/*!***************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FListItem.tsx&modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FHeader.tsx&server=false! ***!
+  \***************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./components/ListItem.tsx */ \"(app-client)/./components/ListItem.tsx\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./components/Header.tsx */ \"(app-client)/./components/Header.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRlVzZXJzJTJGTkFUQUxJQSUyRkRvY3VtZW50cyUyRnNwb3RpZnktY2xvbmUlMkZjb21wb25lbnRzJTJGTGlzdEl0ZW0udHN4Jm1vZHVsZXM9JTJGVXNlcnMlMkZOQVRBTElBJTJGRG9jdW1lbnRzJTJGc3BvdGlmeS1jbG9uZSUyRmNvbXBvbmVudHMlMkZIZWFkZXIudHN4JnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEscUtBQW1HO0FBQ25HIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/NjkxZiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIi9Vc2Vycy9OQVRBTElBL0RvY3VtZW50cy9zcG90aWZ5LWNsb25lL2NvbXBvbmVudHMvTGlzdEl0ZW0udHN4XCIpO1xuaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCIvVXNlcnMvTkFUQUxJQS9Eb2N1bWVudHMvc3BvdGlmeS1jbG9uZS9jb21wb25lbnRzL0hlYWRlci50c3hcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FListItem.tsx&modules=%2FUsers%2FNATALIA%2FDocuments%2Fspotify-clone%2Fcomponents%2FHeader.tsx&server=false!\n"));
+
+/***/ })
+
+});
