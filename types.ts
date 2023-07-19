0df5360073d50232ee9383065923ed61f85b9b8a
Natalia <35937974+Nataly4444@users.noreply.github.com>
2023-07-19 16:54:38 +0300

    stripe integration

--- types.ts
+++ types.ts
@@ -1,5 +1,4 @@
-import Stripe from "stripe";
-
+import Stripe from 'stripe';
 
 export interface Song {
     id: string;
@@ -9,15 +8,6 @@ export interface Song {
     song_path: string;
     image_path: string;
 }
-export interface UserDetails {
-    id: string;
-    first_name: string;
-    last_name: string;
-    full_name?: string;
-    avatar_url?: string;
-    billing_address?: Stripe.Address;
-    payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type]
-}
 
 export interface Product {
     id: string;
@@ -26,22 +16,40 @@ export interface Product {
     description?: string;
     image?: string;
     metadata?: Stripe.Metadata;
-
 }
 
 export interface Price {
     id: string;
-    product_id: string;
+    product_id?: string;
     active?: boolean;
     description?: string;
     unit_amount?: number;
     currency?: string;
     type?: Stripe.Price.Type;
-    intercal?: Stripe.Price.Recurring.Interval;
+    interval?: Stripe.Price.Recurring.Interval;
     interval_count?: number;
     trial_period_days?: number | null;
     metadata?: Stripe.Metadata;
     products?: Product;
+}
+
+export interface Customer {
+    id: string;
+    stripe_customer_id?: string;
+}
+
+export interface UserDetails {
+    id: string;
+    first_name: string;
+    last_name: string;
+    full_name?: string;
+    avatar_url?: string;
+    billing_address?: Stripe.Address;
+    payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface ProductWithPrice extends Product {
+    prices?: Price[];
 }
 
 export interface Subscription {
@@ -49,7 +57,7 @@ export interface Subscription {
     user_id: string;
     status?: Stripe.Subscription.Status;
     metadata?: Stripe.Metadata;
-    price_id: string;
+    price_id?: string;
     quantity?: number;
     cancel_at_period_end?: boolean;
     created: string;
@@ -57,8 +65,8 @@ export interface Subscription {
     current_period_end: string;
     ended_at?: string;
     cancel_at?: string;
-    canceled_at: string;
+    canceled_at?: string;
     trial_start?: string;
-    trial_end: string;
+    trial_end?: string;
     prices?: Price;
 }
